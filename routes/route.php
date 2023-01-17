6f2fdf3d9dd7e4b2d16e1572cdea8cc4617ae385
Mustapha <mustapha@FBISs-MacBook-Pro-3.local>
2023-01-17 14:27:09 +0100

    Resetting route

--- routes/route.php
+++ routes/route.php
@@ -5,6 +5,7 @@ use App\Controllers\ProductController;
 
 $routes = [
     '/' => [ProductController::class, 'index'],
+    'product/all' => [ProductController::class, 'index'],
     '/product/all' => [ProductController::class, 'index'],
     '/product/saveApi' => [ProductController::class, 'store'],
     '/product/deleteApi' => [ProductController::class, 'deleteMany'],
